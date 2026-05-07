849a92de86ffe9d9c989977464ad243e990d0147
Thanush Nethsika <106293021+ThanushDev@users.noreply.github.com>
2026-05-07 19:25:57 +0530

    Switch to React 18 createRoot for rendering

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import "./index.css";
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client"; // මේක වෙනස් කළා
 import { App } from "./App";
 
-render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container!); 
+root.render(<App />);
