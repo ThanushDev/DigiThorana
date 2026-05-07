69b699f1494376cf82483b57ff9e8a798e0023a2
Thanush Nethsika <106293021+ThanushDev@users.noreply.github.com>
2026-05-07 19:19:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages වල රිපොසිටරි එකේ නම මෙතනට දෙන්න
+  base: '/DigiThorana/', 
 })
